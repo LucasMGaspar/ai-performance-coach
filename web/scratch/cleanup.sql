45fcdb647cd7324ef2e9559e6f94293e6d33df60
LucasMGaspar <lucasmanoel.g.g@gmail.com>
2026-05-12 10:08:07 -0300

    fix(build): remover arquivos de scratch que causavam erro no Vercel

--- web/scratch/cleanup.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM "DietLog" WHERE "userId" IN (SELECT id FROM "User" WHERE name ILIKE '%Lucas%');
-DELETE FROM "WorkoutLog" WHERE "userId" IN (SELECT id FROM "User" WHERE name ILIKE '%Lucas%');
-DELETE FROM "DailyCheckIn" WHERE "userId" IN (SELECT id FROM "User" WHERE name ILIKE '%Lucas%');
-DELETE FROM "ExercisePR" WHERE "userId" IN (SELECT id FROM "User" WHERE name ILIKE '%Lucas%');
